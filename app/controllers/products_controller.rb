c4ed9d41d0052ff8ecb5a45968146444409a4452
Abhilash A K <mailto.abhilashak@gmail.com>
2025-04-01 23:54:33 +0530

    Adding images to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.expect(product: [ :title, :description, :category, :color, :size, :mrp, :discount, :rating ])
+      params.expect(product: [ :title, :description, :category, :color, :size, :mrp, :discount, :rating, images: [] ])
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_rich_text :description
+
+  has_many_attached :images do |attachable|
+    attachable.variant :normal, resize_to_limit: [ 540, 720 ]
+    attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
+  end
 end
